125bc96f8fb85e4321bc69d1e7c1eb02d9c234d1
ChenHui <>
2018-10-24 23:55:22 +0800

    updata cadre

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ App({
     // var logs = wx.getStorageSync('logs') || []
     // logs.unshift(Date.now())
     // wx.setStorageSync('logs', logs)
-
     // 登录
     wx.login({
       success: res => {
@@ -13,24 +12,28 @@ App({
         console.log(res)
       }
     })
+    var that = this;
     // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        console.log(res)
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+    wx.getUserInfo({
+      withCredentials: true,
+      success: function (res) {
+        //此处为获取微信信息后的业务方法
+        that.globalData.userInfo = res.userInfo
+      },
+      fail: function () {
+        //获取用户信息失败后。请跳转授权页面
+        wx.showModal({
+          title: '警告',
+          content: '尚未进行授权，请点击确定跳转到授权页面进行授权。',
+          success: function (res) {
+            if (res.confirm) {
+              console.log('用户点击确定')
+              wx.navigateTo({
+                url: '../tologin/tologin',
+              })
             }
-          })
-        }
+          }
+        })
       }
     })
   },
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,123 +1,73 @@
-const App = getApp();
+var App = getApp();
 //按钮打卡页面
 var util = require('../../utils/util.js')
-// var seconds = 10
-// var ing //定时器
 Page({
   data: {
-    encrypt: '',
     wxName: '',
     avatar: '',
     btnStr: '打卡',
-    touchBled: true
+    timer: '',
+    touchBled: true,
+    resultComment: "本阶段目标10000步"    
   },
-  onLoad: function (options) {
-    this.getUserInfo((info) => {
-      this.setData({
-        encrypt: info.belongsTo._id,
-        wxName: info.wxName,
-        avatar: info.img
-      })
-    })
+  onLoad: function(options) {
+
   },
-  onReady: function () {
-    // 页面渲染完成
-    
+  onReady: function() {
+    // 页面初始化 options为页面跳转所带来的参数
+    let that = this;
+    // 以下两个是测试数据
+    let totalItems = 10000;
+    let rightItems = 5000;
+    // let completePercent = parseInt((rightItems / totalItems) * 100);
+    // that.getResultComment(completePercent);
+    that.showScoreAnimation(rightItems, totalItems);
   },
-  onShow: function () {
+  onShow: function() {
     // 页面显示
     this.startTime()
-    console.log(App.globalData)
+    wx.getUserInfo({
+      success: res => {
+        this.setData({
+          wxName: res.userInfo.nickName,
+          avatar: res.userInfo.avatarUrl
+        })
+      }
+    })
   },
-  onHide: function () {
+  onHide: function() {
     // 页面隐藏
   },
-  onUnload: function () {
+  onUnload: function() {
     // 页面关闭
   },
-
-  //获取个人信息  来判断是否打过卡
-  getUserInfo: function (cb) {
-    // wx.request({
-    // url: ,
-    // data: {},
-    // method: 'GET',
-    // success: function (res) {
-    //   // success
-    //   console.log(res)
-    //   typeof cb == 'function' && cb(res.data)
-    // }
-    // })
-  },
-
   //打卡按钮
-
-  toClock: function () {
+  toClock: function() {
     wx.showNavigationBarLoading()
-    // ing = setInterval(() => {
-    //   this.sleepOneMinute()
-    // }, 1000);
-    console.log("2")
-    // this.setData({
-    //   touchBled: false
-    // })
-    // wx.getLocation({
-    //   type: 'wgs84',
-    //   success: (res) => {
-    //     console.log('location', res)
-    //     this.setData({
-    //       touchBled: true
-    //     })
-    //     //this.punch(res.latitude, res.longitude)
-    //   },
-    //   fail: (res) => {
-    //     console.log('location', res)
-    //     wx.hideNavigationBarLoading()
-    //     this.setData({
-    //       info: '打卡功能需要获取您的地理位置信息，请稍后重试'
-    //     })
-    //     wx.getSetting({
-    //       success: (res) => {
-    //         if (res.authSetting['scope.userLocation'] != undefined && res.authSetting['scope.userLocation'] != true) { //非初始化进入该页面,且未授权
-    //           wx.showModal({
-    //             title: '是否授权当前位置',
-    //             content: '需要获取您的地理位置，请确认授权，否则无法获取您所需数据',
-    //             success: function (res) {
-    //               if (res.cancel) {
-    //                 wx.showToast({
-    //                   title: '授权失败',
-    //                   icon: 'success',
-    //                   duration: 1000
-    //                 })
-    //               } else if (res.confirm) {
-    //                 wx.openSetting({
-    //                   success: function (dataAu) {
-    //                     if (dataAu.authSetting["scope.userLocation"] == true) {
-    //                       wx.showToast({
-    //                         title: '授权成功',
-    //                         icon: 'success',
-    //                         duration: 1000
-    //                       })
-    //                     } else {
-    //                       wx.showToast({
-    //                         title: '授权失败',
-    //                         icon: 'success',
-    //                         duration: 1000
-    //                       })
-    //                     }
-    //                   }
-    //                 })
-    //               }
-    //             }
-    //           })
-    //         }
-    //       }
-    //     })
-    //   }
-    // })
-  },
+    console.log(App)
+    if (!App.globalData.userInfo) {
+      //获取用户信息失败后。请跳转授权页面
+      wx.showModal({
+        title: '警告',
+        content: '尚未进行授权，请点击确定跳转到授权页面进行授权。',
+        success: function(res) {
+          if (res.confirm) {
+            console.log('用户点击确定')
+            wx.navigateTo({
+              url: '../tologin/tologin',
+            })
+          }
+        }
+      })
+      return false;
+    }
+    this.setData({
+      "touchBled": false
+    })
+    wx.hideNavigationBarLoading()
 
-  startTime: function () {
+  },
+  startTime: function() {
     var today = new Date();
     var month = today.getMonth();
     var day = today.getDate();
@@ -136,32 +86,103 @@ Page({
       day: day,
       week: util.translateWeek(week)
     })
-    // var t = setTimeout(() => {
-    //   this.startTime()
-    // }, 500);
+    var t = setTimeout(() => {
+      this.startTime()
+    }, 500);
   },
 
-  checkTime: function (i) {
+  checkTime: function(i) {
     if (i < 10) {
       i = "0" + i;
     }
     return i;
   },
 
-  sleepOneMinute: function () {
-    // if (this.data.touchBled == true) {
-    //   // seconds--
-    //   // this.setData({
-    //   //   btnStr: (seconds + ' 秒后可再打卡')
-    //   // })
-    //   // if (seconds == 0) {
-    //   //   this.setData({
-    //   //     btnStr: '打卡',
-    //   //     touchBled: false,
-    //   //   })
-    //   //   // seconds = 10
-    //   //   // clearInterval(ing)
-    //   // }
-    // }
-  }
+  showScoreAnimation: function(rightItems, totalItems) {
+    /*
+    cxt_arc.arc(x, y, r, sAngle, eAngle, counterclockwise);
+    x	                    Number	  圆的x坐标
+    y	                    Number	  圆的y坐标
+    r	                    Number	  圆的半径
+    sAngle	            Number	  起始弧度，单位弧度（在3点钟方向）
+    eAngle	            Number	  终止弧度
+    counterclockwise	    Boolean	  可选。指定弧度的方向是逆时针还是顺时针。默认是false，即顺时针。
+    */
+    let that = this;
+    let copyRightItems = 0;
+    // that.setData({
+      // timer: setInterval(function() {
+        // copyRightItems = copyRightItems+5;
+        if (rightItems >= totalItems) {
+          console.log(copyRightItems)
+          that.setData({
+            resultComment: "本阶段目标已达成",
+            rightItems: rightItems
+          })
+          // clearInterval(that.data.timer)
+        } else {
+          that.setData({
+            rightItems: rightItems
+          })
+          // 页面渲染完成
+          // 这部分是灰色底层
+          let cxt_arc = wx.createCanvasContext('canvasArc'); //创建并返回绘图上下文context对象。
+          cxt_arc.setLineWidth(8); //绘线的宽度
+          cxt_arc.setStrokeStyle('#d2d2d2'); //绘线的颜色
+          cxt_arc.setLineCap('round'); //线条端点样式
+          cxt_arc.beginPath(); //开始一个新的路径
+          cxt_arc.arc(120, 120, 115, 0, 2 * Math.PI, false); //设置一个原点(53,53)，半径为50的圆的路径到当前路径
+          cxt_arc.stroke(); //对当前路径进行描边
+          //这部分是蓝色部分
+          cxt_arc.setLineWidth(8);
+          cxt_arc.setStrokeStyle('#3ea6ff');
+          cxt_arc.setLineCap('round')
+          cxt_arc.beginPath(); //开始一个新的路径
+          cxt_arc.arc(120, 120, 115, -Math.PI * 1 / 2, 2 * Math.PI * (rightItems / totalItems) - Math.PI * 1 / 2, false);
+          cxt_arc.stroke(); //对当前路径进行描边
+          cxt_arc.draw();
+        }
+      // }, 1)
+    // })
+  },
+  getResultComment: function(completePercent) {
+    let that = this;
+    switch (true) {
+      case completePercent < 60:
+        that.setData({
+          resultComment: "渣渣"
+        })
+        break;
+      case completePercent >= 60 && completePercent <= 69:
+        that.setData({
+          resultComment: "学弱"
+        })
+        break;
+      case completePercent >= 70 && completePercent < 80:
+        that.setData({
+          resultComment: "中等"
+        })
+        break;
+      case completePercent >= 80 && completePercent < 90:
+        that.setData({
+          resultComment: "良好"
+        })
+        break;
+      case completePercent >= 90 && completePercent < 95:
+        that.setData({
+          resultComment: "优秀"
+        })
+        break;
+      case completePercent >= 95 && completePercent < 100:
+        that.setData({
+          resultComment: "学霸"
+        })
+        break;
+      case completePercent >= 100:
+        that.setData({
+          resultComment: "学神"
+        })
+        break;
+    }
+  },
 })
